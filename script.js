f0bb2ee45082624b6e5c017523f12d02d6225131
ishicororo <ishicororo.tks@gmail.com>
2025-05-18 20:06:46 +0900

    素数判定の実装

--- script.js
+++ script.js
@@ -3,14 +3,32 @@ const contents={
      prime_check : document.getElementById("prime_check"),
      prime_create : document.getElementById("prime_create"),
      prime_code : document.getElementById("prime_code"),
-};
+}
 function show(content){
      document.getElementsByClassName("open")[0].classList.add("close");
      document.getElementsByClassName("open")[0].classList.remove("open");
      content.classList.add("open");
      content.classList.remove("close");
-};
+}
 function check(){
      const number=document.getElementById("check").value;
      const result=document.getElementById("checked");
+     let isprime=true;
+     for(let prime of primes){
+          if(prime<=Math.sqrt(number)){
+               if(number%prime){
+                    isprime=false;
+                    break;
+               }
+          }
+          else{
+               break;
+          }
+     }
+     if(isprime){
+          result.textContent="素数です。";
+     }
+     else{
+          result.textContent="素数ではありません。";
+     }
 }
