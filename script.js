76609fc9152aea77b33db313509b7fd341674615
ishicororo <ishicororo.tks@gmail.com>
2025-05-18 12:43:39 +0900

    jsの編集

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const home=document.getElementById("home");
-const prime
+const contents={
+     home : document.getElementById("home"),
+     prime_check : document.getElementById("prime_check"),
+     prime_create : document.getElementById("prime_create"),
+     prime_code : document.getElementById("prime_code"),
+};
